b4dc590c98db9fd19c679dd2f24c89ecb6f610c5
syh <61004856+yuhengfdada@users.noreply.github.com>
2020-02-15 16:51:09 +0800

    Update 160.py

--- 160.py
+++ 160.py
@@ -1,5 +1,5 @@
 #一开始想着反向推，结果不对。我佛了。
-#O(n^2)的解法（tm超时了）
+#O(mn)的解法（tm超时了）
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         preserved = headB
@@ -11,4 +11,18 @@ class Solution:
                 headB = headB.next
             headA = headA.next
         return None
-       
+#O(m+n)的解法
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        currentA = headA
+        currentB = headB
+        hashmap = {}
+        while currentA:
+            hashmap[id(currentA)] = currentA
+            currentA = currentA.next
+        while currentB:
+            if id(currentB) in hashmap:
+                return hashmap[id(currentB)]
+            currentB = currentB.next
+        return None
+#O(min(m,n))的艺术解法
