e10bfb23f98d577da12612d8bdc808ac78e09382
syh <61004856+yuhengfdada@users.noreply.github.com>
2020-02-17 11:53:35 +0800

    Update 20.py

--- 20.py
+++ 20.py
@@ -20,3 +20,21 @@ class Solution {
 }
 
 #注：python的list其实具有stack的很多特点。
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                if len(stack) == 0: return False
+                if stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+            else:
+                stack.append(char)
+        return len(stack) == 0
