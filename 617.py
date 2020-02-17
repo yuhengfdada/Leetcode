6ac699b8f8bbf420cf9259563b462ac148efaf54
syh <61004856+yuhengfdada@users.noreply.github.com>
2020-02-17 10:52:43 +0800

    Create 617.py

---
+++ 617.py
@@ -0,0 +1,19 @@
+#要利用好return值啊
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (!t1) {
+            return t2;
+        }
+        if (!t2) {
+            return t1;
+        }
+        // 先合并根节点
+        t1->val =  t1->val + t2->val;
+        // 再递归合并左右子树
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+        
+    }
+};
