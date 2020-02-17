b01b6e94a39be53c6e82363db3e75dafcba96d76
syh <61004856+yuhengfdada@users.noreply.github.com>
2020-02-17 11:33:43 +0800

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,22 @@
+#经典嗷。stack完美契合这题要求。
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (stack.size() == 0) {
+                stack.push(aChar);
+            } else if (isSym(stack.peek(), aChar)) {
+                stack.pop();
+            } else {
+                stack.push(aChar);
+            }
+        }
+        return stack.size() == 0;
+    }
+    private boolean isSym(char c1, char c2) {
+        return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
+    }
+}
+
+#注：python的list其实具有stack的很多特点。
